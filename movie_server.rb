99df39e53152915c9284ae38d62e5f8bfaa45666
Daniel Garland <daniel@Daniels-MacBook-Air.local>
2013-04-03 16:00:50 -0400

    Converting the Movie Grabber into a Sinatra app

---
+++ movie_server.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+require_relative 'movie'
+
+get '/' do
+  # Just have a nice welcome page
+end
+
+post '/film' do
+  # Search for a Movie
+  # HINT - what is in params ?
+  Movie.get_film_info(name)
+
+  # Display the movie in the page
+end
--- movies.rb
+++ movies.rb
@@ -1,18 +1,26 @@
+require 'httparty'
+require 'json'
+
 class Movie
 
   # Add attr_accessors for the values you want to store...
+  attr_accessor :title, :year, :rating, :genre, :director, :actors, :plot, :poster_url
 
   def self.get_film_info(name)    
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
-    binding.pry
-
+    
     # Create a Movie object...
     m = Movie.new
-
-    # Fill in the attributes...
-
-    # Store me in a SQLite3 database...    
+    m.title = movie_info["Title"]
+    m.year = movie_info["Year"]
+    m.rating = movie_info["Rating"]
+    m.genre = movie_info["Genre"]
+    m.director = movie_info["Director"]
+    m.actors = movie_info["Actors"].split(",")
+    m.plot = movie_info["Plot"]
+    m.poster_url = movie_info["Poster"]
+    m
   end
 
 end
